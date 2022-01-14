352237c4a15dabd7f939d461896b314d9721d7e1
krishshah249 <52072651+krishshah249@users.noreply.github.com>
2022-01-14 18:56:48 -0500

    Updated app file

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ def predict():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="https://krish-flight-fare-prediction.herokuapp.com", port=port)
+    app.run(host="0.0.0.0", port=port)
